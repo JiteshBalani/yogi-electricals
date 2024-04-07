11cef20293d41f406ac83f7038d35ce9dc1ecc4f
JiteshBalani <balani.jitesh911@gmail.com>
2024-04-08 03:41:52 +0530

    Service component revamped with new info and added with help of props.

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,44 +1,23 @@
+import { services } from "../utils/services";
 
 const Services = () => {
     return (
         <div id="services" className='mx-auto max-w-screen-xl md:px-6 lg:px-6 py-3 bg-black text-white mb-5'>
             <h1 className='text-2xl md:text-3xl lg:text-4xl text-center py-5 border-b-2 font-semibold'>Services</h1>
             <div className='p-10 flex flex-wrap space-y-10'>
-                <div className='bg-white bg-opacity-10 p-5 rounded-xl  '>
-                    <h2 className='text-lg md:text-xl lg:text-2xl font-semibold  mb-5 px-2 py-2 text-center w-full border-b-2 '>Residential Projects</h2>
-                    <div className='md:flex lg:flex justify-center md:justify-between lg:justify-between items-center'>
-                        <img src="https://live.staticflickr.com/224/516111783_0bcc375c2b_b.jpg" alt='Residential Projects' className='w-3/4 md:w-48 lg:w-48 border-2 mx-auto md:mr-5 lg:mr-5' />
-                        <p className='text-lg md:text-xl lg:text-2xl my-5 text-wrap py-5 text-center md:text-left lg:text-left'>We undertake electrification and MEP work of residential projects.</p>
+
+                {services.map((service, index) => (
+                    <div key={index} className='bg-white bg-opacity-10 p-5 rounded-xl'>
+                        <h2 className='text-lg md:text-xl lg:text-2xl font-semibold  mb-5 px-2 py-2 text-center w-full border-b-2 '>{service.title}</h2>
+                        <div className='md:flex lg:flex justify-center md:justify-between lg:justify-between items-center'>
+                            <img src={service.image} alt={service.title} className='w-3/4 md:w-60 lg:w-60 h-60 border-2 mx-auto md:mr-5 lg:mr-5'></img>
+                            <p className='text-lg md:text-xl lg:text-2xl my-5 text-wrap py-5 text-center md:text-left lg:text-left'>{service.description}</p>
+                        </div>
                     </div>
-                </div>
-                <div className='bg-white bg-opacity-10 p-5 rounded-xl '>
-                    <h2 className='text-lg md:text-xl lg:text-2xl font-semibold text-center mb-5 px-2 py-2 border-b-2'>Commercial Projects</h2>
-                    <div className='md:flex lg:flex justify-center md:justify-between lg:justify-between items-center'>
-                        <img src="https://upload.wikimedia.org/wikipedia/en/4/42/IL%26FS_-_Bandra_Kurla_Complex%2C_Mumbai.jpg" alt='Commercial Projects' className='w-3/4 md:w-48 lg:w-48 border-2 mx-auto md:mr-5 lg:mr-5' />
-                        <p className='text-lg md:text-xl lg:text-2xl my-5 text-wrap py-5 text-center md:text-left lg:text-left'>We are specialized in HVAC, HT-LT panel, DG set commercial installations.</p>
-                    </div>
-                </div>
-                <div className='bg-white bg-opacity-10 p-5 rounded-xl '>
-                    <h2 className='text-lg md:text-xl lg:text-2xl font-semibold text-center mb-5 px-2 py-2 border-b-2'>Industrial Projects</h2>
-                    <div className='md:flex lg:flex justify-center md:justify-between lg:justify-between items-center'>
-                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTff32r_SogqXobO50L5vjQPdvPETJZuSgp3MinMLvdYA&s" alt='Industrial Projects' className='w-3/4 md:w-48 lg:w-48 border-2 mx-auto md:mr-5 lg:mr-5' />
-                        <p className='text-lg md:text-xl lg:text-2xl my-5 text-wrap py-5 text-center md:text-left lg:text-left'>We provide turnkey industrial electrification solutions with maximum safety.</p>
-                    </div>
-                </div>
-                <div className='bg-white bg-opacity-10 p-5 rounded-xl '>
-                    <h2 className='text-lg md:text-xl lg:text-2xl font-semibold text-center mb-5 px-2 py-2 border-b-2'>Power Transmission & Distribution</h2>
-                    <div className='md:flex lg:flex justify-center md:justify-between lg:justify-between items-center'>
-                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFuHn_w77uIlOQrJQqj-apXtG0mzboH6iB2JzZWoVAUw&s" alt='Power Transmission & Distribution' className='w-3/4 md:w-48 lg:w-48 border-2 mx-auto md:mr-5 lg:mr-5' />
-                        <p className='text-lg md:text-xl lg:text-2xl my-5 text-wrap py-5 text-center md:text-left lg:text-left'>We provide substation, power distribution and transmission services.</p>
-                    </div>
-                </div>
-                <div className='bg-white bg-opacity-10 p-5 rounded-xl '>
-                    <h2 className='text-lg md:text-xl lg:text-2xl font-semibold text-center mb-5 px-2 py-2 border-b-2'>Solar Panel Installation</h2>
-                    <div className='md:flex lg:flex justify-center md:justify-between lg:justify-between items-center'>
-                        <img src="https://www.constructionworld.in/assets/uploads/53432dbe0bc73681d8588135da5a2a699b9.jpg" alt='Solar' className='w-3/4 md:w-48 lg:w-48 border-2 mx-auto md:mr-5 lg:mr-5' />
-                        <p className='text-lg md:text-xl lg:text-2xl my-5 text-wrap py-5 text-center md:text-left lg:text-left'>We undertake electrification and MEP work of residential projects.</p>
-                    </div>
-                </div>
+                ))}
+
+                <p className="text-lg md:text-xl lg:text-2xl my-5 text-wrap py-5 text-center md:text-left lg:text-left italic">Have other needs or questions? Reach out to us - we're ready to assist!....</p>
+
             </div>
         </div>
     )
---
+++ src/utils/services.js
@@ -0,0 +1,52 @@
+export const services = [
+    {
+        "title" : "Solar Panel Installation",
+        "image" :"https://4.imimg.com/data4/QG/IR/MY-8144218/solar-energy-projects-500x500.jpg",
+        "description":"Whether for residential, commercial, or industrial applications, trust us to deliver innovative solutions that empower you to embrace a greener future while reducing your carbon footprint. With a commitment to client satisfaction and quality service, we handle every aspect of the installation process with meticulous attention to detail. From site assessment to system design and installation, we prioritize efficiency, reliability, and sustainability.",
+    },
+    {
+        "title" : "HT/LT overhead lines",
+        "image" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFuHn_w77uIlOQrJQqj-apXtG0mzboH6iB2JzZWoVAUw&s",
+        "description":"Experience superior quality services for HT/LT overhead line equipment installation tailored to meet the specific demands of the power industry. Our expertise lies in seamless installation of overhead transmission segments. We prioritize client satisfaction by delivering effective services to diverse industrial and commercial projects. With great tools and technologies, we guarantee flawless execution of HT & LT Installation Services.",
+    },
+    {
+        "title" : "HT/LT underground cables",
+        "image" :"https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvcHgxMzMzNTg1LWltYWdlLWt3eXFvamIzLmpwZw.jpg",
+        "description": "Embark on a journey of reliable HT/LT underground cable services spanning across India. Our commitment to client satisfaction and top-notch service quality remains unparalleled. With the aid of our proficient professionals, we execute these services with utmost precision, finding applications in various industrial and process control systems. Rest assured, our services are delivered at optimized costs within stipulated timeframes.",
+    },
+    {
+        "title" : "HT/LT panels and breakers",
+        "image" :"http://pratibhaelectricals.in/images/services/panel-breakers.jpg",
+        "description":"Unlock the potential of your electrical systems with our annual maintenance contract for HT/LT panels & breakers. We specialize in ensuring the uninterrupted operation of your electrical infrastructure, guaranteeing efficiency and longevity.",
+    },
+    {
+        "title" : "Transformers",
+        "image" :"https://live.staticflickr.com/2572/4099435263_7a7230851e_b.jpg",
+        "description":"Yogi Electricals offers comprehensive testing, maintenance, and repair services for electrical substations and distribution equipment such as transformers. Our services encompass a wide range of tasks including fluid sampling, visual inspection, leak repairs, oil re-refining, electrical testing, reconditioning, and more. Trust us to keep your transformers in optimal condition, ensuring reliability and safety.",
+    },
+    {
+        "title" : "Bus ducts",
+        "image" :"https://4.imimg.com/data4/LG/AU/MY-16136584/square-d-bus-duct.jpg",
+        "description":"Count on Yogi Electricals for the repair and maintenance of bus ducts nationwide. Our dedicated team ensures the seamless operation of your bus duct systems, minimizing downtime and maximizing efficiency.",
+    },
+    {
+        "title" : "DG sets",
+        "image" :"https://3.imimg.com/data3/CV/FX/MY-2176327/diesel-fuel-generator-set.jpg",
+        "description":"Experience peace of mind with our range of DG set services tailored to meet your needs. From annual maintenance contracts to operation and maintenance contracts, as well as supply, testing, commissioning, installation, repairing, and major overhauling of DG sets, we've got you covered. With genuine spares and expert technicians, we ensure the smooth operation of your DG sets.",
+    },
+    {
+        "title" : "11/22/33 KV Substation",
+        "image" :"https://3.imimg.com/data3/GX/EH/MY-3701374/electrical-substation-installation-services-500x500.jpg",
+        "description":"Rely on our extensive industry experience for the maintenance of Substation Work (33/11 KV). Our competent professionals utilize their knowledge and skills efficiently to ensure the protection and automation of all types of electrical substations. With a proven track record of catering to various client needs, we offer cost-effective solutions for optimal substation performance.",
+    },
+    {
+        "title" : "Earthing stations",
+        "image" :"https://5.imimg.com/data5/SELLER/Default/2023/3/295113785/VY/SJ/IB/34991962/earthing-contractor-500x500.webp",
+        "description":"Trust us for the installation, repair, and maintenance of earthing stations designed to enhance safety. Our range of GI earthing station devices provides protection from lightning and electric shocks, ensuring a safer environment for all.",
+    },
+    {
+        "title" : "Lighting and ventilations",
+        "image" :"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhxGCTkx6c0X0VQ-GDswRnbfH-yCWoT7jGssUqwWkTQ7liXc6DyXkWdmb54b1N9QR_-Q4&usqp=CAU",
+        "description":"Enhance your industrial work environment with our lighting and ventilation solutions. Designed to create a fresh and productive shop floor atmosphere, our solutions prioritize efficiency and comfort.",
+    },
+]
